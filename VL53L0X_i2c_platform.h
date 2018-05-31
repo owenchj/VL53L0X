27faed6abe162398b11ff92e0a1c3419c5bb30df
Jie Chen <owenchj@gmail.com>
2018-05-31 09:42:39 +0200

    Fix naming error

--- VL53L0X_i2c_platform.h
+++ VL53L0X_i2c_platform.h
@@ -53,7 +53,7 @@ typedef unsigned char bool_t;
 
 
 #define	   I2C_DEVICE         0x01
-#define	   SPI                0x00
+#define	   SPI_DEVICE         0x00
 
 #define    COMMS_BUFFER_SIZE    64  // MUST be the same size as the SV task buffer
 
